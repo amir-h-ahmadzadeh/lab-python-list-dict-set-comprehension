995448e7b02ccca9e3fe6034e9022ad895da7988
Amir_h_Ahmadzade <a.h.a9372@gmail.com>
2023-08-24 16:01:32 +0200

    "final version"

--- lab-python-comprehension-Amirhossein.py
+++ lab-python-comprehension-Amirhossein.py
@@ -2,18 +2,15 @@ products = ["t-shirt", "mug", "hat", "book", "keychain"]
 inventory = {}
 customer_orders = set()
 def initialize_inventory(products_list):
-  for item in products_list:
-    inventory[item]= int(input(f"How many {item}s are there in the inventory?"))
+  
+  inventory = {item:int(input(f"How many {item}s are there in the inventory?")) for item in products_list}
   return inventory
 
 def get_customer_orders():
 
-    continue_ordering = True
-    while continue_ordering:
-        customer_orders.add((input(f"what is your order? ")))
-        next_order = input(f"Was that all?(yes/no)")
-        if next_order == 'yes':
-            continue_ordering = False
+
+    number_of_orders = int(input("How many items would you like to order?"))
+    customer_orders = {input(f"what is the #{order_counter+1} order? ") for order_counter in range(number_of_orders)}
     return customer_orders
 
 def calculate_order_statistics(inventory, customers_orders):
@@ -24,8 +21,10 @@ def calculate_order_statistics(inventory, customers_orders):
     return order_stat
 
 def update_inventory(inventory, customers_orders):
-   for product in customer_orders:
-      inventory[product] -= 1
+   updated_inventory = {key:(value-1 if key in customers_orders else value) for (key,value) in inventory.items()}
+   updated_inventory = {key:value for (key,value) in updated_inventory.items() if updated_inventory[key] >0}
+   return updated_inventory
+
 
 def print_order_statistics(order_stat):
     print("Order Statistics:")
@@ -33,17 +32,21 @@ def print_order_statistics(order_stat):
     print(f"Percentage of Products Ordered: {order_stat[1] * 100}%")
 
 def print_updated_inventory(inventory):
-    print("Updated inventory has:")
-    for product,quantity in inventory.items():
-        print(f"{product}:{quantity}")
+    output = '\n'.join(f"{product}:{quantity}" for product,quantity in inventory.items() )
+    print("Updated inventory has:",'\n' ,output)
+
+def calculate_total_price(customer_orders):
+    prices = [int(input(f"what's the price of a {item}?")) for item in customer_orders]
+    total_price = sum(prices)
+    print(f"Total price is: {total_price}")
 
 
-
-initialize_inventory(products)
+inventory = initialize_inventory(products)
 customer_orders = get_customer_orders()
 order_statistic = calculate_order_statistics(inventory, customer_orders)
 print_order_statistics(order_statistic)
-update_inventory(inventory,customer_orders)
+inventory = update_inventory(inventory,customer_orders)
 print_updated_inventory(inventory)
+calculate_total_price(customer_orders)
 
 
